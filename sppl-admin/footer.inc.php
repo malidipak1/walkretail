0be8101f4059a562d0bfd100bd812bf343d4f950
Dipak Mali <malidipak1@gmail.com>
2015-04-17 20:29:29 +0530

    deleted folder

--- sppl-admin/footer.inc.php
+++
@@ -1,5 +0,0 @@
-<table width="100%" border="1" cellpadding="0" cellspacing="0" bgcolor="#D6E7F6" style="border-top:solid 1px #333333;">
-  <tr>
-    <td height="20" bgcolor="#4c9309">&nbsp;</td>
-  </tr>
-</table>
